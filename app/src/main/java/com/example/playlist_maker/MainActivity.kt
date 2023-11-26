8ebbc6a186e9f743181cd152a19f3ec3ee0895c3
G-dev-ui <koroliov2108@mail.ru>
2023-11-26 17:37:10 +0300

    Задача спринта 8

--- app/src/main/java/com/example/playlist_maker/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.playlist_maker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val displayButton1 = findViewById<Button>(R.id.botton1)
+        displayButton1.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val displayButton2 = findViewById<Button>(R.id.botton2)
+        displayButton2.setOnClickListener {
+            val intent = Intent(this, MediaActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val displayButton3 = findViewById<Button>(R.id.botton3)
+        displayButton3.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlist_maker/SettingsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.playlist_maker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val backButton = findViewById<ImageView>(R.id.image)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
