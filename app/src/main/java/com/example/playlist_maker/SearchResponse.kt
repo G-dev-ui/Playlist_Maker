b0b7d5251e4d08095a9a21974f58d51e8c4c6f57
G-dev-ui <koroliov2108@mail.ru>
2024-02-14 10:49:20 +0300

    Задача спринта 13

--- app/src/main/java/com/example/playlist_maker/SearchResponse.kt
+++ app/src/main/java/com/example/playlist_maker/SearchResponse.kt
@@ -11,5 +11,9 @@ data class Result(
     @SerializedName("trackName") val trackName: String,
     @SerializedName("artistName") val artistName: String,
     @SerializedName("trackTimeMillis") val trackTimeMillis: Long,
-    @SerializedName("artworkUrl100") val artworkUrl100: String
+    @SerializedName("artworkUrl100") val artworkUrl100: String,
+    @SerializedName("collectionName") val collectionName: String,
+    @SerializedName("releaseDate") val releaseDate: String,
+    @SerializedName("primaryGenreName") val primaryGenreName: String,
+    @SerializedName("country") val country: String
 )
